3c4b8d7be655f8ccbad312d6d1a3b05b1ca36bc4
760173603@qq.com <760173603@qq.com>
2025-03-18 10:41:50 +0800

    inference

--- inference/code/test_binary.py
+++ inference/code/test_binary.py
@@ -5,7 +5,7 @@ from vllm import LLM, SamplingParams
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 # 读取验证数据
 df = pd.read_csv('dataset/data/test_data.csv')
@@ -86,7 +86,7 @@ print(f"F1 Score: {f1}")
 report = classification_report(true_labels, predicted_labels)
 print(report)
 # 将报告保存到文件
-with open('result/lambda-0-val/report.txt', 'w') as f:
+with open('report.txt', 'w') as f:
     f.write(f"Accuracy: {accuracy}\n")
     f.write(f"Precision: {precision}\n")
     f.write(f"Recall: {recall}\n")
@@ -101,4 +101,4 @@ print("报告已保存到 'report.txt'")
 df['predicted_label'] = predicted_labels
 df['generated_text'] = generated_texts
 # df.to_csv('sft_test_results_with_predictions.csv', index=False)
-df.to_csv('result/lambda-0-val/results_with_predictions.csv', index=False)
+df.to_csv('results_with_predictions.csv', index=False)
